c59dacec01ec113dec743d78224f0092fe49262b
Lemonzz-lab <241897354@qq.com>
2025-03-20 14:36:59 +0800

    Update Untitled-1.py to fix a bug

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,26 @@
-print("hello world")
+
+
+import matplotlib.pyplot as plt
+import numpy as np 
+
+x = np.linspace(0, 2 * np.pi, 100)
+y = np.cos(x)
+
+plt.rcParams['font.sans-serif']=['SimHei'] #加上这一句就能在图表中显示中文
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+plt.title(r'$f(x)=cos(x)$') 
+plt.plot(x, y)
+
+plt.figure()
+x1 = np.arange(0,20,1)
+y1 = np.zeros_like(x1)
+y1[10] = 1
+y1[5] = -1
+y1[0] = 1
+y1[15] = -1
+plt.stem(x1,y1)
+
+plt.title(r'离散信号') 
+
+plt.grid(True)
+plt.show()
