300116c56d536d8e25af71682f91eee0eca3d55d
Lemonzz-lab <241897354@qq.com>
2025-03-20 14:38:56 +0800

    teat1

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,4 @@
 
-
 import matplotlib.pyplot as plt
 import numpy as np 
 
@@ -7,7 +6,7 @@ x = np.linspace(0, 2 * np.pi, 100)
 y = np.cos(x)
 
 plt.rcParams['font.sans-serif']=['SimHei'] #加上这一句就能在图表中显示中文
-plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+plt.rcParams['axes.unicode_minus']=False #正常显示负号
 plt.title(r'$f(x)=cos(x)$') 
 plt.plot(x, y)
 
